e66fd0ad9eac4562703956f64969618439ff0e80
Azadova Indira <aindira2197@gmail.com>
2026-01-07 11:33:16 +0500

    Update comments and modify list elements

---
+++ main.py
@@ -0,0 +1,40 @@
+#1-misol
+roy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(roy[3])
+
+
+#2-misol
+roy = ["olma", "banan", "apelsin", "nok", "uzum"]
+print(roy[0:5:4])
+
+
+#3-misol
+roy = ["Nozila", "Sevinch", "Dilnura", "Karina", "Nasiba"]
+roy[2] = "Akmal"
+print(roy)
+
+
+#4-misol
+roy = [10, 20, 30, 40, 50]
+roy[4] = 100
+print(roy)
+
+
+#5-misol
+roy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(roy[1::2])
+
+
+#6-misol
+roy = [5, 10, 15, 20, 25, 30]
+print(roy[:3])
+
+
+#7-misol
+roy = ["qizil", "yashil", "ko'k", "sariq", "qora"]
+print(roy[3:])
+
+
+# #9-misol
+# roy = [100, 200, 300, 400, 500]
+# print(roy[::-1])
